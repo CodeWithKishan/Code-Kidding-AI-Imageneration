32e72bb8d87c4209e6cced2f9083ea5082720719
Kishan <88575007+CodeWithKishan@users.noreply.github.com>
2023-04-24 19:27:34 +0530

    Removed alert

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -28,7 +28,8 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://dall-e-ejrd.onrender.com/api/v1/dalle', {
+        // const response = await fetch('https://dall-e-ejrd.onrender.com/api/v1/dalle', {
+        const response = await fetch('https://dall-e-brtd.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -56,7 +57,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('https://dall-e-ejrd.onrender.com/api/v1/post', {
+        const response = await fetch('https://dall-e-brtd.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -65,7 +66,6 @@ const CreatePost = () => {
         });
 
         await response.json();
-        alert('Success');
         navigate('/');
       } catch (err) {
         alert(err);
